859e77629a861a320aceb1387a1400efb5ac3dcb
Lanvin Kevin <lanvin@a15p10.fil.univ-lille1.fr>
2014-12-02 10:48:08 +0100

    JTextArea added to view

--- src/view/CenterPanel.java
+++ src/view/CenterPanel.java
@@ -1,21 +1,24 @@
 package view;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class CenterPanel extends JPanel {
 
 	protected MainPanel mainPanel;
+	protected JScrollPane scrollPane;
 	protected JTextArea text;
 
 	public CenterPanel(MainPanel mainPanel) {
 		this.mainPanel = mainPanel;
-		this.text = new JTextArea(50,10);
+		this.text = new JTextArea(20,65);
+		this.scrollPane = new JScrollPane(this.text);
+		this.add(scrollPane);
 	}
 	
 	public CenterPanel(MainPanel mainPanel, String text) {
 		this(mainPanel);
 		this.text.setText(text);
 	}
-
 }
